f5e92c29e90eba9b97302172d4fc0fb160518d64
Philippe <fphilfont@gmail.com>
2024-07-25 11:34:33 +0200

    clean and agreg dvf

--- dvf/dvf.clean.R
+++ dvf/dvf.clean.R
@@ -24,6 +24,18 @@ save_dvf_parquet <- function(annee){
   data[,valeur_fonciere := gsub(pattern = ",",replacement = ".",x = valeur_fonciere)][,valeur_fonciere := as.numeric(valeur_fonciere)]
   data[,annee := annee]
   write_parquet(data,paste0("dvf/data/valeurs_foncieres-",annee,".parquet"))
-  
 }
+save_dvf_parquet(2019)
+save_dvf_parquet(2020)
+save_dvf_parquet(2021)
+save_dvf_parquet(2022)
+save_dvf_parquet(2023)
 
+data19 <- as.data.table(read_parquet("dvf/data/valeurs_foncieres-2019.parquet"))
+data20 <- as.data.table(read_parquet("dvf/data/valeurs_foncieres-2020.parquet"))
+data21 <- as.data.table(read_parquet("dvf/data/valeurs_foncieres-2021.parquet"))
+data22 <- as.data.table(read_parquet("dvf/data/valeurs_foncieres-2022.parquet"))
+data23 <- as.data.table(read_parquet("dvf/data/valeurs_foncieres-2023.parquet"))
+
+data <- rbind(data19,data20,data21,data22,data23)
+write_parquet(data,"dvf/data/valeurs_foncieres-2019-2023.parquet")
